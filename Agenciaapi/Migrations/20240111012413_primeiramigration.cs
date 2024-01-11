80589bb10c0ce8ac5a364bdebeea4dd6afc15dae
Josiane Martins <martins.josie90@gmail.com>
2024-01-10 23:19:52 -0300

    primeiro comit api

---
+++ Agenciaapi/Migrations/20240111012413_primeiramigration.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Agenciaapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class primeiramigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nomeCliente = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    cpfCliente = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    enderecoCliente = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    telefoneCliente = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    emailCliente = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Cliente",
+                columns: new[] { "Id", "cpfCliente", "emailCliente", "enderecoCliente", "nomeCliente", "telefoneCliente" },
+                values: new object[,]
+                {
+                    { 1, "111.895.787-11", "01@hotmail", "bla bla bla", "Ana", "1111" },
+                    { 2, "222.895.787-11", "02@hotmail", "logo ali", "Joana", "2222" },
+                    { 3, "333.895.787-11", "03@hotmail", "bla bla bla", "Mara", "3333" },
+                    { 4, "444.895.787-11", "04@hotmail", "logo ali", "Anderson", "4444" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
---
+++ Agenciaapi/Models/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Agenciaapi.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string  nomeCliente { get; set; }
+
+        [StringLength(30)]
+        public string cpfCliente { get; set; }
+
+        [StringLength(50)]
+        public string enderecoCliente { get; set; }
+
+        [StringLength(50)]
+        public string telefoneCliente { get; set; }
+
+        [StringLength(50)]
+        public string emailCliente { get; set; }
+    }
+}
